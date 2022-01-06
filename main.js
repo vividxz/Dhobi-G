0e0b62de30fc439a4d834f3440151c56c45f2b43
vividxz <80575342+vividxz@users.noreply.github.com>
2022-01-06 17:25:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let x = document.getElementById('login');
+let y = document.getElementById('signup');
+let z = document.getElementById('btn');
+
+console.log(z);
+function register() {
+  x.style.left = "-400px";
+  y.style.left = "50px";
+  z.stlye.left = "110px";
+}
+
+function login() {
+  x.style.left = "50px";
+  y.style.left = "450px";
+  z.stlye.left = "0";
+}
